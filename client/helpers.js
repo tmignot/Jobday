8dee3bb6c7e5e2f28d3a537b1a36b51ebf7a8cbb
Thomas Mignot <tmignot@student.42.fr>
2016-10-10 14:58:23 +0200

    Profil - Design

--- client/helpers.js
+++ client/helpers.js
@@ -1,3 +1,17 @@
 Template.registerHelper('userData', function() {
 	return UsersDatas.findOne({userId: Meteor.userId()});
 });
+
+Template.registerHelper('formatDate', function(date) {
+	moment.locale('fr');
+	return moment(date).format('DD MMMM YYYY');
+});
+
+Template.registerHelper('lastLogin', function() {
+	var user = Meteor.user();
+	moment.locale('fr');
+	if (user && user.services && user.services.resume && user.services.resume.loginTokens){
+		var d = _.last(user.services.resume.loginTokens).when;
+		return moment(d).fromNow();
+	}
+});
--- client/javascript/profilUSer.js
+++ client/javascript/profilUSer.js
@@ -1,3 +1,17 @@
+Template.dashboardJobber.onRendered(function() {
+	$('#dispo-calendar').fullCalendar({
+		contentHeight: 250
+	});
+});
 
 Template.dashboardJobber.helpers({
+	userAddress: function() {
+		var u = Meteor.user();
+		if (u) {
+			var d = UsersDatas.findOne({userId: u._id});
+			if (d && d.address && d.address.city)
+				return d.address.city;
+		}
+		return 'Non renseigne';
+	}
 });

fce4f37208c83408c2f4bbf1edc32eb6b3f9cdbf
Thomas Mignot <tmignot@student.42.fr>
2016-11-09 18:21:25 +0100

    map markers

--- client/template/advertCard.js
+++ client/template/advertCard.js
@@ -1,3 +1,28 @@
+Template.advertCard.onRendered(function() {
+	if (this.data) {
+		var addr = this.data.address.city;
+		var name = this.data._id;
+		Maps.onLoad(function() {
+			Maps.create({type: 'geocoder',
+				after: function() {
+					Maps.geocoder.geocode({address: addr}, function(r,s) {
+						if (s == 'OK') {
+							Maps.create({type: 'marker',
+								name: name,
+								map: Maps.maps.searchMissionMap,
+								position: {
+									lat: r[0].geometry.location.lat(),
+									lng: r[0].geometry.location.lng()
+								}
+							});
+						}
+					})
+				}
+			});
+		});
+	}
+});
+
 Template.advertCard.helpers({
 	getCat: function() { //returns [category][subcategory]
 		var d = Template.instance().data;
